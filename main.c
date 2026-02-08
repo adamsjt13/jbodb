e665662390f93e982b3b9b4940b167db5e8826b3
adamsjt13 <adamsjt13@gmail.com>
2026-02-08 11:33:17 -0600

    adding REPL and simple command print outs

--- main.c
+++ main.c
@@ -1,34 +1,67 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_options() {
-    printf("Incorrect usage! Please use one of the following:\n");
+void print_options(int error) {
+    if(error == 1) {
+        printf("Invalid Input!\n\n");
+    }
+    printf("Please use one of the following:\n");
     printf("1. PUT <key> <value>\n");
     printf("2. GET <key>\n");
     printf("3. DEL <key>\n");
+    printf("4. EXIT\n");
 }
 
-int main(int argc, char* argv[]) {
+int main() {
     printf("Welcome to JboDB!\n");
+    print_options(0);
 
-    if (argc < 2) {
-        print_options();
-        return 1;
-    }
+    while(1) {
 
+        char line[256];
+        printf(">");
+        
+        if (fgets(line, sizeof(line), stdin) != NULL) {
 
-    if(strcmp(argv[1], "PUT") == 0){
-        printf("OK! Inserting (%s, %s) into db!\n", argv[2], argv[3]);
+            line[strcspn(line, "\n")] = '\0';
+            
+            char *command = strtok(line, " ");
+            char * arg1 = strtok(NULL, " ");
+            char * arg2 = strtok(NULL, " ");
+
+            if(strcmp(command, "PUT") == 0){
+                if(arg1 && arg2 && !strtok(NULL, " ")) {
+                    printf("OK! Inserting (%s, %s) into db!\n", arg1, arg2);
+                } else {
+                print_options(1);
+                continue;
+                }
+            }
+            else if(strcmp(command, "GET") == 0){
+                if(arg1 && !arg2) {
+                    printf("OK! Retrieving value for key, %s, from db!\n", arg1);
+                } else {
+                print_options(1);
+                continue;
+                }
+            }
+            else if(strcmp(command, "DEL") == 0){
+                if(arg1 && !arg2) {
+                    printf("OK! Deleting key, %s, from db!\n", arg1);
+                } else {
+                print_options(1);
+                continue;
+                }
+            }
+            else if(strcmp(command, "EXIT") == 0){
+                if(!arg1) {
+                    break;
+                }
+            }
+            else
+                print_options(1);
+                continue;
+            }
+
     }
-    else if(strcmp(argv[1], "GET") == 0){
-        printf("OK! Retrieving value for key, %s, from db!\n", argv[2]);
-    }
-    else if(strcmp(argv[1], "DEL") == 0){
-        printf("OK! Deleting key, %s, from db!\n", argv[2]);
-    }
-    else {
-        print_options();
-        return 1;
-    }
-    return 0;
 }
